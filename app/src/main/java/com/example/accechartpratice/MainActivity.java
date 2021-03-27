561a9142c93448d1171c2b8fb267f4db7e4abf06
TQ <0076c031@email.ntou.edu.tw>
2021-03-27 21:54:14 +0800

    Add landscape mode.
    Update in next version
    - Add Marker

--- app/src/main/java/com/example/accechartpratice/MainActivity.java
+++ app/src/main/java/com/example/accechartpratice/MainActivity.java
@@ -2,13 +2,13 @@ package com.example.accechartpratice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -25,28 +25,41 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private final static String TAG = "MainActivity";
 
-    private SensorManager sensorManager;//給Sensor管理員
-    private Sensor mAcce;//給加速規
+    //Sensor管理員
+    private SensorManager sensorManager;
 
-    //宣告三軸的LineChart
+    //加速規
+    private Sensor mAcce;
+
+    //儲存螢幕當前方向
+    private int screenOrientation;
+
+    //螢幕直向時的三軸的LineChart
     private LineChart lineChartX;
     private LineChart lineChartY;
     private LineChart lineChartZ;
 
-    private Button rearange;
+    //螢幕橫向時的綜合LineChart
+    private LineChart lineChartLand;
+
+    private Button resetChart;
     private TextView acceText;
 
-    private int maxDataCount = 250;
+    private static final int maxDataCount = 500;
     private int curCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initLayoutComponents();
-        initSensor();
-        initChart();
-        initText();
+        initSensor();//初始化感測器
+        initLayoutComponents();//初始化圖表以外元件
+        screenOrientation = this.getResources().getConfiguration().orientation;
+        if (screenOrientation == Configuration.ORIENTATION_PORTRAIT) {
+            initChart();
+        } else {
+            initChartLand();
+        }
     }
 
     @Override
@@ -70,16 +83,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     private void initLayoutComponents() {
-        rearange = (Button) findViewById(R.id.rearange);
-        rearange.setOnClickListener(new View.OnClickListener() {
+        resetChart = (Button) findViewById(R.id.rearange);
+        acceText = (TextView) findViewById(R.id.acceInfo);
+        acceText.setText("加速規訊息處理中....");
+        resetChart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                lineChartX.fitScreen();
-                lineChartY.fitScreen();
-                lineChartZ.fitScreen();
+                chartFitScreen();
             }
         });
-        rearange.setOnLongClickListener(new View.OnLongClickListener() {
+        resetChart.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
                 resetChart();
@@ -88,12 +101,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         });
     }
 
+    private void chartFitScreen(){
+        if(screenOrientation == Configuration.ORIENTATION_PORTRAIT){
+            lineChartX.fitScreen();
+            lineChartY.fitScreen();
+            lineChartZ.fitScreen();
+        }
+        else{
+            lineChartLand.fitScreen();
+        }
+    }
+
     private void resetChart() {
-        lineChartX.clear();
-        lineChartY.clear();
-        lineChartZ.clear();
-        curCounter = 0;
-        initChart();
+        if (screenOrientation == Configuration.ORIENTATION_PORTRAIT) {
+            lineChartX.clear();
+            lineChartY.clear();
+            lineChartZ.clear();
+            curCounter = 0;
+            initChart();
+        } else {
+            lineChartLand.clear();
+            curCounter = 0;
+            initChartLand();
+        }
     }
 
     private void initSensor() {
@@ -107,21 +137,77 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         int red[] = {255, 0, 0};
         int green[] = {0, 255, 0};
         int blue[] = {0, 0, 255};
+            //取得LineChart元件
+            lineChartX = (LineChart) findViewById(R.id.AcceChartX);
+            lineChartY = (LineChart) findViewById(R.id.AcceChartY);
+            lineChartZ = (LineChart) findViewById(R.id.AcceChartZ);
 
+            //資料陣列
+            ArrayList<Entry> arrayX = new ArrayList<>();
+            ArrayList<Entry> arrayY = new ArrayList<>();
+            ArrayList<Entry> arrayZ = new ArrayList<>();
+
+            //創建空的以利生成
+//            arrayX.add(new Entry());
+//            arrayY.add(new Entry());
+//            arrayZ.add(new Entry());
+
+            //創建LineDataSet
+            LineDataSet lineDataSetX = new LineDataSet(arrayX, "acceX");
+            LineDataSet lineDataSetY = new LineDataSet(arrayY, "acceY");
+            LineDataSet lineDataSetZ = new LineDataSet(arrayZ, "acceZ");
+
+            //不要畫製圈圈
+            lineDataSetX.setDrawCircles(false);
+            lineDataSetY.setDrawCircles(false);
+            lineDataSetZ.setDrawCircles(false);
+
+            //設定線條顏色
+            lineDataSetX.setColor(Color.rgb(244, 57, 144));
+            lineDataSetY.setColor(Color.rgb(17, 213, 104));
+            lineDataSetZ.setColor(Color.rgb(34, 166, 242));
+
+            ArrayList<ILineDataSet> dataSetsX = new ArrayList<>();
+            ArrayList<ILineDataSet> dataSetsY = new ArrayList<>();
+            ArrayList<ILineDataSet> dataSetsZ = new ArrayList<>();
+
+            dataSetsX.add(lineDataSetX);
+            dataSetsY.add(lineDataSetY);
+            dataSetsZ.add(lineDataSetZ);
+
+            LineData lineDataX = new LineData(dataSetsX);
+            LineData lineDataY = new LineData(dataSetsY);
+            LineData lineDataZ = new LineData(dataSetsZ);
+
+            lineChartX.setData(lineDataX);
+            lineChartY.setData(lineDataY);
+            lineChartZ.setData(lineDataZ);
+
+            //設定Legend顏色
+            lineChartX.getLegend().setTextColor(Color.rgb(244, 57, 144));
+            lineChartY.getLegend().setTextColor(Color.rgb(17, 213, 104));
+            lineChartZ.getLegend().setTextColor(Color.rgb(34, 166, 242));
+
+//            removeFirstDataInChart();
+
+            setChartXAxisRange(lineChartX, 0, maxDataCount);
+            setChartXAxisRange(lineChartY, 0, maxDataCount);
+            setChartXAxisRange(lineChartZ, 0, maxDataCount);
+            setChartYAxisProperties(lineChartX);
+            setChartYAxisProperties(lineChartY);
+            setChartYAxisProperties(lineChartZ);
+
+
+    }
+
+    private void initChartLand() {
         //取得LineChart元件
-        lineChartX = (LineChart) findViewById(R.id.XAcce);
-        lineChartY = (LineChart) findViewById(R.id.YAcce);
-        lineChartZ = (LineChart) findViewById(R.id.ZAcce);
+        lineChartLand = (LineChart) findViewById(R.id.AcceChartLand);
 
         //資料陣列
         ArrayList<Entry> arrayX = new ArrayList<>();
         ArrayList<Entry> arrayY = new ArrayList<>();
         ArrayList<Entry> arrayZ = new ArrayList<>();
-
-        //創建空的以利生成
-        arrayX.add(new Entry());
-        arrayY.add(new Entry());
-        arrayZ.add(new Entry());
 
         //創建LineDataSet
         LineDataSet lineDataSetX = new LineDataSet(arrayX, "acceX");
@@ -138,108 +224,102 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         lineDataSetY.setColor(Color.rgb(17, 213, 104));
         lineDataSetZ.setColor(Color.rgb(34, 166, 242));
 
-        ArrayList<ILineDataSet> dataSetsX = new ArrayList<>();
-        ArrayList<ILineDataSet> dataSetsY = new ArrayList<>();
-        ArrayList<ILineDataSet> dataSetsZ = new ArrayList<>();
+        ArrayList<ILineDataSet> dataSetsLand = new ArrayList<>();
 
-        dataSetsX.add(lineDataSetX);
-        dataSetsY.add(lineDataSetY);
-        dataSetsZ.add(lineDataSetZ);
+        dataSetsLand.add(lineDataSetX);
+        dataSetsLand.add(lineDataSetY);
+        dataSetsLand.add(lineDataSetZ);
 
-        LineData lineDataX = new LineData(dataSetsX);
-        LineData lineDataY = new LineData(dataSetsY);
-        LineData lineDataZ = new LineData(dataSetsZ);
+        LineData lineDataLand = new LineData(dataSetsLand);
 
-        lineChartX.setData(lineDataX);
-        lineChartY.setData(lineDataY);
-        lineChartZ.setData(lineDataZ);
+        lineChartLand.setData(lineDataLand);
 
-        lineChartX.getLegend().setTextColor(Color.rgb(244, 57, 144));
-        lineChartY.getLegend().setTextColor(Color.rgb(17, 213, 104));
-        lineChartZ.getLegend().setTextColor(Color.rgb(34, 166, 242));
+        //設定Legend顏色
+        lineChartLand.getLegend().setTextColor(Color.rgb(235,235,235));
 
-        removeFirstDataInChart();
-
-        setChartXAxisRange(0, maxDataCount);
-        setChartYAxisRange();
+        setChartXAxisRange(lineChartLand, 0, maxDataCount);
+        setChartYAxisProperties(lineChartLand);
     }
 
-    private void setChartXAxisRange(float Minimum, float Maximum) {
-
-        lineChartX.getXAxis().setAxisMinimum(Minimum);
-        lineChartX.getXAxis().setAxisMaximum(Maximum);
-
-        lineChartY.getXAxis().setAxisMinimum(Minimum);
-        lineChartY.getXAxis().setAxisMaximum(Maximum);
-
-        lineChartZ.getXAxis().setAxisMinimum(Minimum);
-        lineChartZ.getXAxis().setAxisMaximum(Maximum);
+    private void setChartXAxisRange(LineChart lineChart, float Minimum, float Maximum) {
+        lineChart.getXAxis().setAxisMinimum(Minimum);
+        lineChart.getXAxis().setAxisMaximum(Maximum);
     }
 
-    private void setChartYAxisRange() {
+    private void setChartYAxisProperties(LineChart lineChart) {
         //自動縮放Y軸
-        lineChartX.setAutoScaleMinMaxEnabled(true);
-        lineChartY.setAutoScaleMinMaxEnabled(true);
-        lineChartZ.setAutoScaleMinMaxEnabled(true);
-//        //y軸下限
-//        lineChartX.getAxisLeft().setAxisMinimum(-20);
-//        lineChartY.getAxisLeft().setAxisMinimum(-20);
-//        lineChartZ.getAxisLeft().setAxisMinimum(-20);
-//
-//        //y軸上限
-//        lineChartX.getAxisLeft().setAxisMaximum(20);
-//        lineChartY.getAxisLeft().setAxisMaximum(20);
-//        lineChartZ.getAxisLeft().setAxisMaximum(20);
+//       lineChart.setAutoScaleMinMaxEnabled(true);
+
+        //y軸下限
+        lineChart.getAxisLeft().setAxisMinimum(-20);
+
+        //y軸上限
+        lineChart.getAxisLeft().setAxisMaximum(20);
 
         //不要輸出值
-        lineChartX.getLineData().setDrawValues(false);
-        lineChartY.getLineData().setDrawValues(false);
-        lineChartZ.getLineData().setDrawValues(false);
+        lineChart.getLineData().setDrawValues(false);
 
         //不要顯示Description label
-        lineChartX.getDescription().setEnabled(false);
-        lineChartY.getDescription().setEnabled(false);
-        lineChartZ.getDescription().setEnabled(false);
+        lineChart.getDescription().setEnabled(false);
 
         //左方Y軸文字顏色
-        lineChartX.getAxisLeft().setTextColor(Color.rgb(235, 245, 255));
-        lineChartY.getAxisLeft().setTextColor(Color.rgb(235, 245, 255));
-        lineChartZ.getAxisLeft().setTextColor(Color.rgb(235, 245, 255));
+        lineChart.getAxisLeft().setTextColor(Color.rgb(235, 245, 255));
 
         //上方文字
-        lineChartX.getXAxis().setTextColor(Color.rgb(235, 245, 255));
-        lineChartY.getXAxis().setTextColor(Color.rgb(235, 245, 255));
-        lineChartZ.getXAxis().setTextColor(Color.rgb(235, 245, 255));
+        lineChart.getXAxis().setTextColor(Color.rgb(235, 245, 255));
 
         //右方Y軸不顯示
-        lineChartX.getAxisRight().setDrawLabels(false);
-        lineChartY.getAxisRight().setDrawLabels(false);
-        lineChartZ.getAxisRight().setDrawLabels(false);
+        lineChart.getAxisRight().setDrawLabels(false);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
 
         acceText.setText("X軸 : " + event.values[0] + "\nY軸 : " + event.values[1] + "\nZ軸 : " + event.values[2]);
+        if (screenOrientation == Configuration.ORIENTATION_PORTRAIT) {
+            if (curCounter >= maxDataCount) {
+//            removeFirstDataInChart();
+                setChartXAxisRange(lineChartX, (float) (curCounter - maxDataCount), (float) curCounter);
+                setChartXAxisRange(lineChartY, (float) (curCounter - maxDataCount), (float) curCounter);
+                setChartXAxisRange(lineChartZ, (float) (curCounter - maxDataCount), (float) curCounter);
+            } else {
+                setChartXAxisRange(lineChartX, 0, (float) maxDataCount);
+                setChartXAxisRange(lineChartY, 0, (float) maxDataCount);
+                setChartXAxisRange(lineChartZ, 0, (float) maxDataCount);
+            }
 
-        if (curCounter >= maxDataCount) {
-            removeFirstDataInChart();
-            setChartXAxisRange((float) (curCounter - maxDataCount), (float) curCounter);
-        } else {
-            setChartXAxisRange(0, (float) maxDataCount);
+            lineChartX.getLineData().getDataSets().get(0).addEntry(new Entry((float) curCounter, event.values[0]));
+            lineChartX.notifyDataSetChanged();
+            lineChartX.invalidate();
+
+            lineChartY.getLineData().getDataSets().get(0).addEntry(new Entry((float) curCounter, event.values[1]));
+            lineChartY.notifyDataSetChanged();
+            lineChartY.invalidate();
+
+            lineChartZ.getLineData().getDataSets().get(0).addEntry(new Entry((float) curCounter, event.values[2]));
+            lineChartZ.notifyDataSetChanged();
+            lineChartZ.invalidate();
+        }else{
+            if (curCounter >= maxDataCount) {
+//            removeFirstDataInChart();
+                setChartXAxisRange(lineChartLand, (float) (curCounter - maxDataCount), (float) curCounter);
+            } else {
+                setChartXAxisRange(lineChartLand, 0, (float) maxDataCount);
+            }
+
+            lineChartLand.getLineData().getDataSets().get(0).addEntry(new Entry((float) curCounter, event.values[0]));
+            lineChartLand.getLineData().getDataSets().get(1).addEntry(new Entry((float) curCounter, event.values[1]));
+            lineChartLand.getLineData().getDataSets().get(2).addEntry(new Entry((float) curCounter, event.values[2]));
+            lineChartLand.notifyDataSetChanged();
+            lineChartLand.invalidate();
+//            lineChartY.getLineData().getDataSets().get(0).addEntry(new Entry((float) curCounter, event.values[1]));
+//            lineChartY.notifyDataSetChanged();
+//            lineChartY.invalidate();
+//
+//            lineChartZ.getLineData().getDataSets().get(0).addEntry(new Entry((float) curCounter, event.values[2]));
+//            lineChartZ.notifyDataSetChanged();
+//            lineChartZ.invalidate();
         }
-
-        lineChartX.getLineData().getDataSets().get(0).addEntry(new Entry((float) curCounter, event.values[0]));
-        lineChartX.notifyDataSetChanged();
-        lineChartX.invalidate();
-
-        lineChartY.getLineData().getDataSets().get(0).addEntry(new Entry((float) curCounter, event.values[1]));
-        lineChartY.notifyDataSetChanged();
-        lineChartY.invalidate();
-
-        lineChartZ.getLineData().getDataSets().get(0).addEntry(new Entry((float) curCounter, event.values[2]));
-        lineChartZ.notifyDataSetChanged();
-        lineChartZ.invalidate();
 
         curCounter++;
 
@@ -250,15 +330,4 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 
-    private void removeFirstDataInChart() {//移除第一筆資料
-
-        lineChartX.getLineData().getDataSets().get(0).removeFirst();
-        lineChartY.getLineData().getDataSets().get(0).removeFirst();
-        lineChartZ.getLineData().getDataSets().get(0).removeFirst();
-    }
-
-    private void initText() {
-        acceText = (TextView) findViewById(R.id.acceInfo);
-        acceText.setText("加速規訊息處理中....");
-    }
 }
